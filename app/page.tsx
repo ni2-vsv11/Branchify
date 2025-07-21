c434ff58447a16faf18155f19808120254b13010
ni2-vsv11 <ni2educational@gmail.com>
2025-07-21 19:04:28 +0530

    Home-page designing Completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section className="bg-[#34d817] min-h-[100vh] grid grid-cols-2">
-        <div className=" flex items-center justify-center">
-         <p className="text-lime-300 font-black text-3xl">Everything you are. In one, simple link in bio.</p>
-         <p>Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+      <section className="bg-[#254F1A] min-h-[100vh] grid grid-cols-2 ">
+        <div className=" flex item-center justify-center flex-col ml-[10vw] gap-3 pt-30">
+         <span className="text-yellow-300 font-black text-7xl">Everything you </span>
+          <span className="text-yellow-300 font-black text-7xl">are. In one,</span> 
+          <span className="text-yellow-300 font-black text-7xl">simple link in bio.</span>
+         <span className="text-yellow-200 text-xl my-4">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</span>
+         
+         <div className="input flex gap-2">
+          <input className="px-2 py-2 rounded-md focus:outline-lime-600 bg-white " type="text" placeholder="branchtr.ee/your-url" />
+          <button className="bg-rose-200 text-black font-semibold rounded-md px-4 py-4">Get your link-branch</button>
+         </div>
         </div>
-        <div className="bg-sky-300">
-          col 2
+        <div className="flex items-center justify-center flex-col mr-[10vw]">
+          <Image src="/Home.jpg" alt="Home-page" width={500} height={500} style={{ borderRadius: '20px' }} />
         </div>
       </section>
 
-      <section className="bg-rose-400 min-h-[100vh]">
+      <section className="bg-rose-200 min-h-[100vh]">
       
       </section>
     </main>
